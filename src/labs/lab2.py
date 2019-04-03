750807988adf0a6323b201a960460a43d823657e
Thiago Santos <s.thiagoh@gmail.com>
2019-04-03 13:56:13 -0300

    Added itens 5 to 8 of lab 2

--- src/labs/lab2.py
+++ src/labs/lab2.py
@@ -3,127 +3,88 @@ import git
 import os
 import collections
 
-def getCommitNumOfFiles(repo, arg):
-  commits = list(repo.iter_commits('master'))
-  commit = commits[arg]
-  return len(commit.stats.files)
+def getJavaLinesCountByCommit(repo, commit):
+  index = repo.index
+  index.reset(commit)
+  co = index.checkout(force=True)
 
-def getCommitNumOfJavaFiles(repo, arg):
-  commits = list(repo.iter_commits('master'))
-  commit = commits[arg]
   count = 0
-  for f in list(commit.stats.files):
-    if ('.java' in f):
-      count = count + 1
+  for file in co:
+    if(".java" in file):
+      f = open(os.getcwd() + "/EventBus/" + file)
+      count += len(f.readlines())
+  
   return count
 
-def getCommitsFileCount(repo):
-  commits = list(repo.iter_commits('master'))
-  for c in commits:
-    print('Commit ' + c.message)
-    print(str(len(c.stats.files)) + ' arquivos modificados')
+def getFileCountByCommit(repo, commit, type=None):
+  index = repo.index
+  index.reset(commit)
+  co = index.checkout(force=True)
 
-def getCommitsJavaFileCount(repo):
-  commits = list(repo.iter_commits('master'))
-  for c in commits:
-    print('Commit ' + c.message)
-    files = c.stats.files
-    count = 0
-    for f in files:
-      if ('.java' in f):
-        count = count + 1
-    print(str(count) + ' arquivos java modificados')
-
-  commits = list(repo.iter_commits('master', since=dateSince, until=dateUntil))
-  fileChanges = []
-  for c in commits:
-    for d in c.diff():
-      fileName = d.a_path
-      if ('.java' in fileName):
-        fileChanges.append(fileName)
-  print(collections.Counter(fileChanges).most_common(5))
-
-def getCommitsJavaLinesCount(repo):
-  commits = list(repo.iter_commits('master'))
-  for c in commits:
-    print('Commit ' + c.message)
-    count = 0
-    for d in c.diff(git.NULL_TREE, create_patch=True):
-      if ('.java' in str(d.a_path)):
-        for line in d.diff.splitlines():
-          print(line)
-          if (line[0] == '+'):
-            count = count + 1
-        print(str(count) + ' linhas java modificadas')
-
-def getAddedFilesByYear(repo, dateSince, dateUntil):
-  commits = repo.iter_commits('master', since=dateSince, until=dateUntil)
-  for c in commits:
-    d = c.diff(git.NULL_TREE, create_patch=True)
-    print(d)
-
-def getAllCommits(repo):
-  return len(list(repo.iter_commits('master')))
-
-def getCommitsByYear(repo, dateSince, dateUntil):
-  return len(list(repo.iter_commits('master', since=dateSince, until=dateUntil)))
-
-def getCommitsByMsg(repo, msg):
-  return len(list(repo.iter_commits('master', grep=msg)))
-
-def getMostModifiedFiles(repo, dateSince='2000-01-01', dateUntil='2050-01-01'):
-  commits = list(repo.iter_commits('master', since=dateSince, until=dateUntil))
-  fileChanges = []
-  for c in commits:
-    for d in c.diff():
-      fileName = d.a_path
-      if ('.java' in fileName):
-        fileChanges.append(fileName)
-  print(collections.Counter(fileChanges).most_common(5))
-
-def getMostActiveAuthorsByDate(repo, dateSince='2000-01-01', dateUntil='2050-01-01'):
-  commits = list(repo.iter_commits('master', since=dateSince, until=dateUntil))
-
-  authors = []
-  for c in commits:
-    authors.append(c.author.name)
-
-  mostActive = collections.Counter(authors).most_common(3)
-  for author, count in mostActive:
-    print(author)
-
-#type='+'-> added
-#type='-' -> removed
-def getAlteredCodeCountByText(repo, text, type='+'):
-  commits = list(repo.iter_commits('master'))
-  lines = []
   count = 0
-  for c in commits:
-    for d in c.diff(git.NULL_TREE, create_patch=True, S=text):
-      for line in d.diff.splitlines():
-        if(line[0] == type and text in line):
-          count = count + 1
+  for file in co:
+    if(type is None or type in file):
+      count += 1
 
-  print (count)
+  return count
+
+#item 5
+def getJavaLinesCountInEachCommit(repo):
+  commits = list(repo.iter_commits('master'))
+  for c in reversed(commits):
+    print c.hexsha
+    print getJavaLinesCountByCommit(repo, c)
+
+#item 6
+def getFileCountInEachYear(repo):
+  for year in range(2012,2020):
+    dateSince = str(year) + "-01-01"
+    dateUntil = str(year) + "-12-31"
+    
+    count = 0 
+    commits = list(repo.iter_commits('master', since=dateSince, until=dateUntil))
+    for c in commits:
+      count += getFileCountByCommit(repo, c)
+
+    print year, ": ", count
+
+#item 7
+def getJavaFileCountInEachYear(repo):
+  for year in range(2012,2020):
+    dateSince = str(year) + "-01-01"
+    dateUntil = str(year) + "-12-31"
+    
+    count = 0 
+    commits = list(repo.iter_commits('master', since=dateSince, until=dateUntil))
+    for c in commits:
+      count += getFileCountByCommit(repo, c, ".java")
+
+    print year, ": ", count
+
+#item 8
+def getJavaLinesCountInEachYear(repo):
+  for year in range(2012,2020):
+    dateSince = str(year) + "-01-01"
+    dateUntil = str(year) + "-12-31"
+    
+    count = 0 
+    commits = list(repo.iter_commits('master', since=dateSince, until=dateUntil))
+    for c in commits:
+      count += getJavaLinesCountByCommit(repo, c)
+
+    print year, ": ", count
+
 
 repo = Repo(os.getcwd() + '/EventBus')
 
-# print("-- ITEM 01 --")
-# print(getCommitNumOfFiles(repo, 0))
-# print(getCommitNumOfFiles(repo, -1))
-
-# print("-- ITEM 02 --")
-# print(getCommitNumOfJavaFiles(repo, 0))
-# print(getCommitNumOfJavaFiles(repo, -1))
-
-# print("-- ITEM 03 --")
-# getCommitsFileCount(repo)
-
-# print("-- ITEM 04 --")
-# getCommitsJavaFileCount(repo)
-
-# print("-- ITEM 05 --")
-# getCommitsJavaLinesCount(repo)
+print("-- ITEM 05 --")
+getJavaLinesCountInEachCommit(repo)
 
 print("-- ITEM 06 --")
-getAddedFilesByYear(repo, '2016-01-01', '2016-12-31')
+getFileCountInEachYear(repo)
+
+print("-- ITEM 07 --")
+getJavaFileCountInEachYear(repo)
+
+print("-- ITEM 08 --")
+getJavaLinesCountInEachYear(repo)
