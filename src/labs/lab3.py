56dbcbe1c2b3d8693145f396b0b99c5a35650a6b
Thiago Santos <s.thiagoh@gmail.com>
2019-04-09 12:36:25 -0300

    added itens 2 to 5 of lab 3

--- src/labs/lab3.py
+++ src/labs/lab3.py
@@ -1,18 +1,19 @@
 from git import Repo
 import os
 import git
+import time
 
 def normalizeFile(filename, old_string="\t", new_string=" "):
   # Safely read the input filename using 'with'
   with open(filename) as f:
       s = f.read()
       if old_string not in s:
-        print('"{old_string}" not found in {filename}.'.format(**locals()))
+        #print('"{old_string}" not found in {filename}.'.format(**locals()))
         return
 
   # Safely write the changed content, if found in the file
   with open(filename, 'w') as f:
-    print('Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals()))
+    #print('Changing "{old_string}" to "{new_string}" in {filename}'.format(**locals()))
     s = s.replace(old_string, new_string)
     f.write(s)
 
@@ -35,25 +36,20 @@ def getFileMetrics(path):
           mca = tabsCount
         break
     # print(str(tabsCount) + " espacos encontrados")
-  cra = float(cta) / float(lineCount)
+  if(lineCount > 0):
+    cra = float(cta) / float(lineCount)
+  else:
+    cra = 0
+
   return cta, cra, mca
 
 def getLastCommit(repo):
   commits = list(repo.iter_commits('master'))
-  commit = commits[0]
-  return commit
+  return commits[0]
 
-def getFileCountByCommit(repo, commit, type=None):
-  index = repo.index
-  index.reset(commit)
-  co = index.checkout(force=True)
-
-  count = 0
-  for file in co:
-    if(type is None or type in file):
-      count += 1
-
-  return count
+def getYearAgoCommit(repo):
+  commits = list(repo.iter_commits('master', until='2018-04-09'))
+  return commits[0]
 
 def getCommitMetrics(commit):
   index = repo.index
@@ -65,7 +61,7 @@ def getCommitMetrics(commit):
   maxMca = 0
 
   for file in co:
-    print(file)
+    #print(file)
     cta, cra, mca = getFileMetrics("EventBus/" + file)
     if (cta > maxCta):
       maxCta = cta
@@ -77,13 +73,54 @@ def getCommitMetrics(commit):
       maxMca = mca
       mcaFile = file
 
+  print('Commit ' + commit.hexsha +
+   ' (' + time.strftime('%d/%m/%Y %H:%M:%S', time.gmtime(commit.committed_date)) + ')')
   print('O arquivo ' + ctaFile + ' possui o maior CTA neste commit (' + str(maxCta) + ')')
   print('O arquivo ' + craFile + ' possui o maior CRA neste commit (' + str(maxCra) + ')')
   print('O arquivo ' + mcaFile + ' possui o maior MCA neste commit (' + str(maxMca) + ')')
 
+
+def getCommitCP(commit):
+  index = repo.index
+  index.reset(commit)
+  co = index.checkout(force=True)
+
+  fileCount = 0
+  cp = 0
+
+  for file in co:
+    cta, cra, mca = getFileMetrics("EventBus/" + file)
+    fileCount += 1
+    cp += cra
+  cp = cp/fileCount
+
+  print('Complexidade do projeto no commit ' + commit.hexsha +
+   ' (' + time.strftime('%d/%m/%Y %H:%M:%S', time.gmtime(commit.committed_date)) + ') : ' 
+   + str(cp))
+
+def getAllCommitsCP(repo):
+  commits = list(repo.iter_commits('master'))
+  for c in reversed(commits) :
+    getCommitCP(c)
+
+
 repo = Repo(os.getcwd() + '/EventBus')
 
 lastCommit = getLastCommit(repo)
+yearAgoCommit = getYearAgoCommit(repo)
+
+
+print("--- ITEM 1 ---")
 getCommitMetrics(lastCommit)
 
-# inplace_change(os.getcwd() + "/teste.py",  '\t', ' ')
+print("\n--- ITEM 2 ---")
+getCommitMetrics(yearAgoCommit)
+
+print("\n--- ITEM 3 ---")
+getCommitCP(lastCommit)
+
+print("\n--- ITEM 4 ---")
+getCommitCP(yearAgoCommit)
+
+print("\n--- ITEM 5 ---")
+getAllCommitsCP(repo)
